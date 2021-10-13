81d24579faf9cee56553227a98f81bfa7acfcfc9
egberts <egberts@github.com>
2021-10-13 09:28:44 -0400

    Server-based packages

---
+++ 200-debian-pkg-deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+sudo apt install devscripts
+
+
---
+++ 210-debian-pkgs-optionals.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo apt install lshw
+sudo apt install pciutils
+sudo apt install usbutils
+sudo apt install dmidecode
+sudo apt install ethtool
+sudo apt install psmisc   # pstree
