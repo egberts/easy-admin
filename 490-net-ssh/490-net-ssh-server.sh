fab784a4cfe7dd0b98b064da33b52baffcc691c8
egberts <egberts@github.com>
2021-11-08 20:27:19 -0500

    Make the script not dependent on 'sudo' Create a build subdirectory

--- 490-net-ssh/490-net-ssh-server.sh
+++ 490-net-ssh/490-net-ssh-server.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#
-# File: 490-net-ssh.sh
+# File: 490-net-ssh-server.sh
 # Title: Setup and harden SSH server
 #
+# Env Vars (and its default setting):
+#   SSHD_CONF="$BUILDROOT/etc/ssh/sshd_config"
+#   ALGORITHM_USED="ed25519"  # or "ecdsa"
+#   BUILDROOT="build"
+#   CHROOT_DIR=""
+#   PREFIX="/usr"
+#   EXEC_PREFIX="/usr"
+#   LOCALSTATEDIR="/"  (Debian, /var for others)
+#   SYSCONFDIR="/etc"
+#   LIBDIR="$PREFIX/lib"
+#
 
-MINI_REPO=${PWD}
+MINI_REPO="./sshd_config.d"
 
-prefix=/usr
-prefix=        # Debian
-# exec_prefix="$prefix"
-# VAR_DIRPATH="$exec_prefix/var"  # Redhat
-# VAR_DIRPATH=""  # Debian
-
-sysconfdir="$prefix/etc"  # Redhat
-sysconfdir="$prefix/etc/ssh"  # Debian
-# localstatedir="$VAR_DIRPATH/run"
+source ./ssh-openssh-common
 
 
 # We are forcing no-root login permitted here
@@ -25,9 +27,13 @@ sysconfdir="$prefix/etc/ssh"  # Debian
 WARNING_NO_ROOT_LOGIN=0
 
 # Check for '!' substring in root entry of /etc/shadow file.
-ROOT_LOGIN_PWD="$(sudo grep root /etc/shadow | awk -F: '{ print $2; }')"
-if [[ "$ROOT_LOGIN_PWD" == *'!'* ]]; then
-  WARNING_NO_ROOT_LOGIN=1
+if [ -r /etc/shadow ]; then
+  ROOT_LOGIN_PWD="$(grep root /etc/shadow | awk -F: '{ print $2; }')"
+  if [[ "$ROOT_LOGIN_PWD" == *'!'* ]]; then
+    WARNING_NO_ROOT_LOGIN=1
+  fi
+else
+  echo "Skipping the check for no-root-login condition..."
 fi
 
 # Check for '*' substring in root entry of /etc/shadow file.
@@ -39,7 +45,8 @@ echo "This host has no direct root login (only sudo)"
 # Check if anyone has 'sudo' group access on this host
 SUDO_USERS_BY_GROUP="$(grep sudo /etc/group | awk -F: '{ print $4; }')"
 if [ -z "$SUDO_USERS_BY_GROUP" ]; then
-  echo "There is no user account in the 'sudo' group; "
+  echo "There is no user account involving with the 'sudo' group; "
+  echo "... You may want to add 'ssh' supplemental group to various users."
 
   # Well, no direct root and no sudo-able user account, this is rather bad.
   if [ $WARNING_NO_ROOT_LOGIN -ne 0 ]; then
@@ -60,30 +67,47 @@ if [ -z "$SSH_USERS_BY_GROUP" ]; then
   exit 1
 fi
 
+# Even if we are root, we abide by BUILDROOT directive as to
+# where the final configuration settings goes into.
+ABSPATH="$(dirname $BUILDROOT)"
+if [ "$ABSPATH" != "." ] && [ "${ABSPATH:0:1}" != '/' ]; then
+  echo "$BUILDROOT is an absolute path, we probably need root privilege"
+  echo "We are backing up old SSH settings"
+  # Only the first copy is saved as the backup
+  if [ ! -f "${SSHD_CONF_FILESPEC}.backup" ]; then
+    BACKUP_FILENAME=".backup-$(date +'%Y%M%d%H%M')"
+    echo "Moving /etc/ssh/* to /etc/ssh/${BACKUP_FILENAME}/ ..."
+    mv "$SSHD_CONF_FILESPEC" "${SSHD_CONF_FILESPEC}.backup"
+    retsts=$?
+    if [ $retsts -ne 0 ]; then
+      echo "ERROR: Failed to create a backup of /etc/ssh/*"
+      exit 3
+    fi
+  fi
+else
+  echo "Creating subdirectories to $BUILDROOT ..."
+  mkdir -p $BUILDROOT
 
-sudo apt install openssh-server
-
-echo "We are blowing away the old SSH settings"
-
-SSHD_CONFIG_FILENAME="sshd_config"
-SSHD_CONFIG_DIRPATH="$sysconfdir"
-SSHD_CONFIG_FILESPEC="$SSHD_CONFIG_DIRPATH/$SSHD_CONFIG_FILENAME"
-# Only the first copy is saved as the backup
-if [ ! -f "${SSHD_CONFIG_FILESPEC}.backup" ]; then
-  mv "$SSHD_CONFIG_FILESPEC" "${SSHD_CONFIG_FILESPEC}.backup"
+  FILE_SETTINGS_FILESPEC="${BUILDROOT}/filemods_openssh_sshd.sh"
+  echo "Creating file permission script in $FILE_SETTINGS_FILESPEC ..."
+  echo "#!/bin/bash" > $FILE_SETTINGS_FILESPEC
+  echo "# File: $(basename $FILE_SETTINGS_FILESPEC)" >> $FILE_SETTINGS_FILESPEC
+  echo "# Path: ${PWD}/$(dirname $FILE_SETTINGS_FILESPEC)" >> $FILE_SETTINGS_FILESPEC
+  echo "# Title: File permission settings for SSH daemon"
 fi
-sudo chown root:ssh "$SSHD_CONFIG_FILESPEC"
-sudo chmod 640 "$SSHD_CONFIG_FILESPEC"
+
+mkdir -p "$BUILDROOT$SSHD_CONFD_DIRSPEC"
+
+flex_chown root:ssh "$SSHD_CONF_FILESPEC"
+flex_chmod 640      "$SSHD_CONF_FILESPEC"
 
 
 # Update the SSH server settings
 #
 
-cat << SSHD_EOF | sudo tee "$SSHD_CONFIG_FILESPEC" >/dev/null 2>&1
-#
-# File: $SSHD_CONFIG_FILENAME
-# Path: $SSHD_CONFIG_DIRPATH
-# Title: SSH server configuration file
+create_script_header "$SSHD_CONF_FILESPEC" "OpenSSH daemon configuration file"
+
+cat << SSHD_EOF | tee "$BUILDROOT$SSHD_CONF_FILESPEC" >/dev/null 2>&1
 #
 # Edition: sshd(8) v8.4p1 compiled-default
 #          OpenSSL 1.1.1k  25 Mar 2021
@@ -129,25 +153,31 @@ cat << SSHD_EOF | sudo tee "$SSHD_CONFIG_FILESPEC" >/dev/null 2>&1
 include "/etc/ssh/sshd_config.d/*.conf"
 SSHD_EOF
 
-SSHD_CONFIG_DIRNAME="sshd_config.d"
-SSHD_CONFIG_DIRSPEC="$sysconfdir/$SSHD_CONFIG_DIRNAME"
-if [ ! -d "$SSHD_CONFIG_DIRSPEC" ]; then
-  sudo mkdir "$SSHD_CONFIG_DIRSPEC"
-fi
-sudo chown root:ssh "$SSHD_CONFIG_DIRSPEC"
-sudo chmod 750 "$SSHD_CONFIG_DIRSPEC"
-sudo cp "$MINI_REPO"/sshd_config.d/* "$SSHD_CONFIG_DIRSPEC"/
+# Create Debian-specific subdirectory for SSH-specific configuration settings
+flex_mkdir "$SSHD_CONFD_DIRSPEC"
+flex_chown root:ssh "$SSHD_CONFD_DIRSPEC"
+flex_chmod 750 "$SSHD_CONFD_DIRSPEC"
+cp "$MINI_REPO"/* "${BUILDROOT}$SSHD_CONFD_DIRSPEC"/
 
-sudo chown root:ssh ${SSHD_CONFIG_DIRSPEC}/*
-sudo chmod 640 ${SSHD_CONFIG_DIRSPEC}/*
+for this_file in $(ls -1 ${SSHD_CONFD_DIRSPEC}/*); do
+  flex_chown root:ssh "$this_file"
+  flex_chmod 640 "$this_file"
+done
 
+# Touch an empty host key file
+ALGORITHM_USED="${ALGORITHM_USED:-ed25519}"
+SSH_KEY_FILESPEC="$ext_ssh_dirspec/ssh_host_${ALGORITHM_USED}_key"
+flex_touch "$SSH_KEY_FILESPEC"
+flex_chown root:ssh "$SSH_KEY_FILESPEC"
+flex_chmod 640 "$SSH_KEY_FILESPEC"
+touch "${BUILDROOT}${SSH_KEY_FILESPEC}"
 
-sudo sshd -T -t >/dev/null 2>&1
+$OPENSSH_SSHD_BIN_FILESPEC -T -t -h ${BUILDROOT}${ext_ssh_dirspec}/ssh_host_ed25519_key -f ${BUILDROOT}${SSHD_CONF_FILESPEC} >/dev/null 2>&1
 retsts=$?
 if [ $retsts -ne 0 ]; then
   echo "Error during ssh config syntax checking."
   echo "Showing sshd_config output"
-  sudo sshd -T -t
+  $OPENSSH_SSHD_BIN_FILESPEC -T -t -h ${BUILDROOT}${ext_ssh_dirspec}/ssh_host_ed25519_key -f ${BUILDROOT}${SSHD_CONF_FILESPEC}
   exit "$retsts"
 fi
 
@@ -155,11 +185,13 @@ fi
 
 FOUND=0
 USERS_IN_SSH_GROUP="$(grep ssh /etc/group | awk -F: '{ print $4 }')"
-for THIS_USER in $USERS_IN_SSH_GROUP; do
-  if [ "${THIS_USER}" == "${USER}" ]; then
-    echo "User ${USER} has access to this hosts SSH server"
-    FOUND=1
-  fi
+for THIS_USERS in $USERS_IN_SSH_GROUP; do
+  for this_user in $(echo "$THIS_USERS" | sed 's/,/ /g' | xargs -n1); do
+    if [ "${this_user}" == "${USER}" ]; then
+      echo "User ${USER} has access to this hosts SSH server"
+      FOUND=1
+    fi
+  done
 done
 
 if [ $FOUND -eq 0 ]; then
@@ -168,7 +200,4 @@ if [ $FOUND -eq 0 ]; then
   echo "  usermod -g ssh ${USER}"
   exit 1
 fi
-exit
-
-sudo systemctl restart ssh.service
 
