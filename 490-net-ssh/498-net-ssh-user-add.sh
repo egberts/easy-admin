8126aa0e81d3582888aad5a22ca6f7f1bb40a835
egberts <egberts@github.com>
2021-11-09 10:54:39 -0500

    Make it work with default value (empty) to pick up its shell's $USER value instead

--- 490-net-ssh/498-net-ssh-user-add.sh
+++ 490-net-ssh/498-net-ssh-user-add.sh
@@ -4,13 +4,20 @@
 
 source ./ssh-openssh-common
 
-read -rp "Add user to '$SSH_GROUP' group [$USER]?: "
+if [ $UID -ne 0 ]; then
+  echo "WARNING: sudo password may appear."
+fi
 
-SSH_USER="$REPLY"
+read -rp "Add the name of user to '$SSH_GROUP' group [$USER]?: "
+if [ -z "$REPLY" ]; then
+  SSH_USER="$USER"
+else
+  SSH_USER="$REPLY"
+fi
 
 # Add user to SSH group: who can log into this host?
 echo "Executing: sudo addgroup $SSH_USER $SSH_GROUP"
-sudo addgroup "$SSH_USER" $SSH_GROUP
+sudo usermod -a -G "$SSH_GROUP" "$SSH_USER"
 RETSTS=$?
 if [ $RETSTS -ne 0 ]; then
   echo "Unable to add '$SSH_USER' user to '$SSH_GROUP' group: Error $RETSTS"
