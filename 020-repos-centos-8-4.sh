67c7e7a244916dfe7b993c1badf3cd59c672b787
Egbert St <egberts@github.com>
2021-12-10 15:35:21 -0500

    CentOS repositories (that CentOS does not tell you about) that are extremely useful.

---
+++ 020-repos-centos-8-4.sh
@@ -0,0 +1,46 @@
+#
+# File: 020-repos-centos-8-4.sh
+#
+# Reference:
+#   - https://www.dedoimedo.com/computers/centos-8-perfect-desktop.html
+
+if [ 0 -ne 0 ]; then
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+sudo dnf install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
+
+sudo dnf install https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+
+#
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+dnf install gh
+fi
+
+# Replace python symlink with python3 in /etc/alternatives
+if [ -f /etc/alternatives/python ]; then
+  REAL_PYTHON="$(readlink /etc/alternatives/python)"
+  if [[ "$REAL_PYTHON" == *no-python* ]]; then
+    sudo rm /etc/alternatives/python
+    sudo ln -s /etc/alternatives/python3 /etc/alternatives/python
+  else
+    echo "Unable to revert python3 to python in /etc/alternatives; skipping"
+  fi
+else
+  sudo ln -s /etc/alternatives/python3 /etc/alternatives/python
+fi
+if [ -f /usr/bin/python ]; then
+  REAL_PYTHON="$(readlink /etc/alternatives/python)"
+  if [[ "REAL_PYTHON" == *no-python* ]]; then
+    echo "no-python found"
+    sudo rm /usr/bin/python
+    sudo ln -s /etc/alternatives/python /usr/bin/python
+  else
+    echo "Unable to revert python3 to python in /usr/bin; skipping"
+    echo "Aborted."
+    exit 11
+  fi
+else
+  sudo ln -s /etc/alternatives/python /usr/bin/python
+fi
+echo ""
+echo "Done."
---
+++ 021-repos-centos-nice-things.sh
@@ -0,0 +1,14 @@
+#
+# File: 021-repos-centos-nice-things.sh
+#
+
+dnf install kde-settings
+dnf install kde-settings-plasma
+dnf install qt-settings
+dnf install kde-gtk-config
+
+dnf install gimp libreoffice-writer steam
+
+dnf install vlc
+
+dnf install "downloaded chrome or skype rpm"
