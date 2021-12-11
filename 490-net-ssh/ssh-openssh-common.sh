eda918ad5583266014c33375e4af1fcf29fe89d6
Egbert St <egberts@github.com>
2021-12-11 09:59:10 -0500

    Auto-create the build subdirectory, if needed

--- 490-net-ssh/ssh-openssh-common.sh
+++ 490-net-ssh/ssh-openssh-common.sh
@@ -12,7 +12,15 @@ BUILD_SSH="${BUILDROOT}/partial-ssh"
 
 source installer.sh
 
-
 DEFAULT_ETC_CONF_DIRNAME="ssh"
 source os-distro.sh
+
+if [ "${BUILDROOT:0:1}" != '/' ]; then
+  # relative build, create directories
+  # absolute build, do not create build directory
+  if [ ! -d "$BUILDROOT" ]; then
+    mkdir -p "$BUILDROOT"
+  fi
+  mkdir -p "${BUILDROOT}$sysconfdir"
+fi
 
