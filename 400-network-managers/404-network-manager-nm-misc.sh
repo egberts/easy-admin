ebe4016158937cda46670d75ac231bce3414bc08
egberts <egberts@github.com>
2022-01-27 12:04:33 -0500

    Incorporate the 'easy-admin-installer.sh' into 404-network-manager-nm-misc.sh

--- 400-network-managers/404-network-manager-nm-misc.sh
+++ 400-network-managers/404-network-manager-nm-misc.sh
@@ -4,12 +4,16 @@
 
 echo "Set up NetworkManager configurations"
 
+source ./maintainer-NetworkManager.sh
+
+FILE_SETTINGS_FILESPEC="file-NetworkManager-conf.d.sh"
+
 echo "Disable randomization of MAC address in Wifi"
 FILENAME="wifi_rand_mac.conf"
 FILEPATH="/etc/NetworkManager/conf.d"
 FILESPEC="$FILEPATH/$FILENAME"
-echo "Writing $FILESPEC..."
-cat << NM_CONF | sudo tee "$FILESPEC"
+echo "Creating ${BUILDROOT}${CHROOT_DIR}/$FILESPEC..."
+cat << NM_CONF | tee "${BUILDROOT}${CHROOT_DIR}/$FILESPEC" > /dev/null
 #
 # File: $FILENAME
 # Path: $FILEPATH
@@ -23,13 +27,20 @@ cat << NM_CONF | sudo tee "$FILESPEC"
 wifi.scan-rand-mac-address=no
 
 NM_CONF
+retsts=$?
+if [ $retsts -ne 0 ]; then
+  echo "Unable to create $FILESPEC; error $retsts"
+  exit $retsts
+fi
+flex_chown root:root "$FILESPEC"
+flex_chmod 0640 "$FILESPEC"
 
 echo "Disable systemd-resolved (resolvconf, et. al.)"
 FILENAME="no-systemd-resolved.conf"
 FILEPATH="/etc/NetworkManager/conf.d"
 FILESPEC="$FILEPATH/$FILENAME"
-echo "Writing $FILESPEC..."
-cat << NM_CONF | sudo tee "$FILESPEC"
+echo "Creating ${BUILDROOT}${CHROOT_DIR}/$FILESPEC..."
+cat << NM_CONF | tee "${BUILDROOT}${CHROOT_DIR}/$FILESPEC" > /dev/null
 #
 # File: $FILENAME
 # Path: $FILEPATH
@@ -43,6 +54,39 @@ cat << NM_CONF | sudo tee "$FILESPEC"
 systemd-resolved=false
 
 NM_CONF
+retsts=$?
+if [ $retsts -ne 0 ]; then
+  echo "Unable to create $FILESPEC; error $retsts"
+  exit $retsts
+fi
+flex_chown root:root "$FILESPEC"
+flex_chmod 0640 "$FILESPEC"
 
+echo "Disabled DHCP-driven DNS update"
+FILENAME="no-dhcp-dns-update-to-resolver.conf"
+FILEPATH="/etc/NetworkManager/conf.d"
+FILESPEC="$FILEPATH/$FILENAME"
+echo "Creating ${BUILDROOT}${CHROOT_DIR}/$FILESPEC..."
+cat << NM_CONF | tee "${BUILDROOT}${CHROOT_DIR}/$FILESPEC" > /dev/null
+#
+# File: $FILENAME
+# Path: $FILEPATH
+# Title: Disable NetworkManager DHCP from updating resolver
+#
+# Creator: $(realpath "$0")
+# Date: $(date)
+#
+
+[main]
+dns=none
+
+NM_CONF
+retsts=$?
+if [ $retsts -ne 0 ]; then
+  echo "Unable to create $FILESPEC; error $retsts"
+  exit $retsts
+fi
+flex_chown root:root "$FILESPEC"
+flex_chmod 0640 "$FILESPEC"
 
 
