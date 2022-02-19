144c766aa20c926eaae9354f802987bbc6a97b5b
egberts <egberts@github.com>
2022-02-19 16:01:08 -0500

    Prevent lockout of non-root due to /etc/nologin file while root account is disabled due to Common Criteria and other security guidelines.

---
+++ 120-core-hardening/015-pam-nologin-lockout.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# File: 015-pam-no-lockout-adm-group.sh
+# Title: Prevent lockout due to /etc/nologin and disabled root account
+# Description:
+#
+echo "Prevent Lockout Due to /etc/nologin & Disabled root Account"
+echo
+
+echo "Add the following in /etc/pam.d/login"
+echo "just right before the 'auth requisite pam_nologin.so ...' line:"
+echo
+
+echo "    # TWEAK (prevents lockout due to /etc/nologin)"
+echo "    # if you want to allow users in the adm group to log in"
+echo "    # on a text console even if /etc/nologin exists"
+echo "    # This would not work through remote term console (sshd)"
+echo "    auth [default=ignore success=1] pam_succeed_if.so quiet user ingroup adm"
+
+echo
+echo "Then add user(s) that can ignore this /etc/nologin toward the 'adm' group:"
+echo
+echo "   groupmod -a -G adm <privileged-username>"
+echo
+echo "Aborted."
