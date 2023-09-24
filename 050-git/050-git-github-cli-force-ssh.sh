eb350211132dc36511bfcc31b46da22ea2274017
egberts <egberts@github.com>
2023-09-24 16:04:09 -0500

    Forced SSH in gh package is broken Disabled

--- 050-git/050-git-github-cli-force-ssh.sh
+++ 050-git/050-git-github-cli-force-ssh.sh
@@ -3,4 +3,4 @@
 # Title: Configure local account for CLI-based auth access with GitHub
 
 # gh config set -h git_protocol ssh  # broken
-gh config set -h git_protocol ssh  -h github.com # broken
+# gh config set -h git_protocol ssh  -h github.com # broken
