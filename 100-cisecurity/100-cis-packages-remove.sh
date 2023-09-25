beeda3eed3fa479584eac94aebee246834531c50
egberts <egberts@github.com>
2023-09-25 17:17:35 -0500

    reformatted for readability

--- 100-cisecurity/100-cis-packages-remove.sh
+++ 100-cisecurity/100-cis-packages-remove.sh
@@ -3,7 +3,6 @@
 # Title: Remove packages as CIS Benchmark recommends
 #
 
-
 echo "CIS Benchmark Recommendation for removals of Debian packages"
 echo ""
 
@@ -12,9 +11,12 @@ if [ "$REPLY" != 'continue' ]; then
   echo "Aborted."
   exit 254
 fi
+
 sudo systemctl --now disable autofs
 sudo apt purge autofs
 sudo apt purge xinetd
 sudo apt purge openbsd-inetd
 sudo apt purge nfs-common
 
+echo "Done."
+exit 0
