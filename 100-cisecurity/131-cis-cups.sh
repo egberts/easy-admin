55ed2a13acab14e80399e86a9f384e8b0efbe6bd
egberts <egberts@github.com>
2023-09-25 09:34:44 -0500

    Add an extra step before removal of CUPS printer service

--- 100-cisecurity/131-cis-cups.sh
+++ 100-cisecurity/131-cis-cups.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# File: 130-cis-avahi.sh
-# Title: Disable AVAHI daemon
+# File: 131-cis-cups.sh
+# Title: Disable CUPS daemon
 #
+echo "Disabling CUPS printer service ..."
+echo
+echo -n "Enter in 'continue' then ENTER to continue: "
+read -r ANYTHING
+if [ "$ANYTHING" != "continue" ]; then
+  echo "Aborted."
+  exit 1
+fi
 
 echo "Checking if cups-daemon is running..."
 cups_active="$(systemctl is-active cups.service 2>/dev/null)"
-if [ $? -eq 0 ] && [ "$cups_active" != 'inactive' ]; then
+RETSTS=$?
+if [ $RETSTS -eq 0 ] && [ "$cups_active" != 'inactive' ]; then
   echo "Disabling and stopping cups.service..."
   systemctl --now stop cups.service
   systemctl --now disable cups.service
