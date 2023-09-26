8500d3657776e65f6a8b64be67c2778662ee8da5
egberts <egberts@github.com>
2023-09-25 23:10:18 -0500

    add file header

--- 020-repos/020-repos-arch.sh
+++ 020-repos/020-repos-arch.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-#
+# File: 020-repos-arch.sh
+# Title: Install what is needed for Arch distro
 # OS: ArchLinux
 # Kernel: 5.10.46
+# Description
+#   Install everything except the Linux kernel
 #
-# Everything except the Linux kernel
+# Files impacted:
+#  read   - /usr/bin/pacman
+#  create - various packages
+#  modify - none
+#  delete - none
+#
+# Environment Variables:
+#   none
+#
+# Prerequisites:
+#   pacman
+#
+
 
 echo "For Arch Linux, Kernel 5.15.46 requires some more packages"
-
+echo
 
 pacman -S bind
 pacman -S chrony
 pacman -S openssh
 pacman -S dhcp
 pacman -S nftables
-
 pacman -S fontconfig
+echo
 
+echo "Done."
